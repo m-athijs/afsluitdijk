29849af84d186bcb167167586a5787d6d14ffbd8
Mathijs van Bockhooven <mathijs@Mathijss-MacBook-Pro.local>
2017-10-25 22:36:41 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+window.applicationCache.addEventListener('cached', function (e) {
+    $("#status").html("<a href='#'>start audio tour</a>");
+}, false);
+
+window.applicationCache.addEventListener('noupdate', function (e) {
+    $("#status").html("<a href='#'>start audio tour</a>");
+}, false);
+
+window.applicationCache.addEventListener('updateready', function (e) {
+    $("#status").html("<a href='#'>start audio tour</a>");
+}, false);
+
+window.applicationCache.addEventListener('checking', function (e) {
+    $("#cacheevent").html("checking " + e.message);
+    $("#cachestatus").html(window.applicationCache.status);
+}, false);
+
+window.applicationCache.addEventListener('downloading', function (e) {
+    $("#cacheevent").html("downloading " + e.message);
+    $("#cachestatus").html(window.applicationCache.status);
+}, false);
+
+window.applicationCache.addEventListener('error', function (e) {
+    $("#cacheevent").html("error " + e.message);
+    $("#cachestatus").html(window.applicationCache.status);
+}, false);
+
+window.applicationCache.addEventListener('obsolete', function (e) {
+    $("#cacheevent").html("obsolete " + e.message);
+    $("#cachestatus").html(window.applicationCache.status);
+}, false);
+
+window.applicationCache.addEventListener('progress', function (e) {
+    $("#cacheevent").html("progress " + e.message);
+    $("#cachestatus").html(window.applicationCache.status);
+}, false);
+
+window.applicationCache.update();
+
+if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+    window.applicationCache.swapCache();
+}
