b91689395d3eea3fb3434e22dcc0be278ff0e26b
Mathijs van Bockhooven <mathijs@Mathijss-MacBook-Pro.local>
2017-10-26 11:25:42 +0200

    clean up

--- main.js
+++ main.js
@@ -11,49 +11,49 @@ $(document).ready(function () {
         $("#status").html("<a href='audiotour1.html'>START AUDIO TOUR</a>");
         $("#language").hide();
         $("#status").show();
-        if (e.message != undefined) {
-            $("#cacheevent").html("checking " + e.message);
-        }
-        $("#cachestatus").html(window.applicationCache.status);
+        // if (e.message != undefined) {
+        //     $("#cacheevent").html("checking " + e.message);
+        // }
+        // $("#cachestatus").html(window.applicationCache.status);
     }, false);
     
     window.applicationCache.addEventListener('noupdate', function (e) {
         $("#status").html("<a href='audiotour1.html'>START AUDIO TOUR</a>");
         $("#language").hide();
         $("#status").show();
-        if (e.message != undefined) {
-            $("#cacheevent").html("checking " + e.message);
-        }
-        $("#cachestatus").html(window.applicationCache.status);
+        // if (e.message != undefined) {
+        //     $("#cacheevent").html("checking " + e.message);
+        // }
+        // $("#cachestatus").html(window.applicationCache.status);
     }, false);
     
     window.applicationCache.addEventListener('updateready', function (e) {
         $("#status").html("<a href='audiotour1.html'>START AUDIO TOUR</a>");
         $("#language").hide();
         $("#status").show();
-        if (e.message != undefined) {
-            $("#cacheevent").html("checking " + e.message);
-        }
-        $("#cachestatus").html(window.applicationCache.status);
+        // if (e.message != undefined) {
+        //     $("#cacheevent").html("checking " + e.message);
+        // }
+        // $("#cachestatus").html(window.applicationCache.status);
     }, false);
     
     window.applicationCache.addEventListener('checking', function (e) {
-        if (e.message != undefined) {
-            $("#cacheevent").html("checking " + e.message);
-        }
-        $("#cachestatus").html(window.applicationCache.status);
+        // if (e.message != undefined) {
+        //     $("#cacheevent").html("checking " + e.message);
+        // }
+        // $("#cachestatus").html(window.applicationCache.status);
     }, false);
     
     window.applicationCache.addEventListener('downloading', function (e) {
-        if (e.message != undefined) {
-            $("#cacheevent").html("downloading " + e.message);
-        }
-        $("#cachestatus").html(window.applicationCache.status);
+        // if (e.message != undefined) {
+        //     $("#cacheevent").html("downloading " + e.message);
+        // }
+        // $("#cachestatus").html(window.applicationCache.status);
     }, false);
     
     window.applicationCache.addEventListener('error', function (e) {
-        $("#cacheevent").html("error " + e.message);
-        $("#cachestatus").html(window.applicationCache.status);
+        // $("#cacheevent").html("error " + e.message);
+        // $("#cachestatus").html(window.applicationCache.status);
         if (window.applicationCache.status === 1) {
             $("#status").html("<a href='audiotour1.html'>START AUDIO TOUR</a>");
             $("#language").hide();
@@ -62,15 +62,15 @@ $(document).ready(function () {
     }, false);
     
     window.applicationCache.addEventListener('obsolete', function (e) {
-        $("#cacheevent").html("obsolete " + e.message);
-        $("#cachestatus").html(window.applicationCache.status);
+        // $("#cacheevent").html("obsolete " + e.message);
+        // $("#cachestatus").html(window.applicationCache.status);
     }, false);
     
     window.applicationCache.addEventListener('progress', function (e) {
-        $("#cacheevent").html("progress " + e.message);
+        // $("#cacheevent").html("progress " + e.message);
         var percentageLoaded = Math.ceil(e.loaded / e.total * 100);
         $("#loaded").html(percentageLoaded);
-        $("#cachestatus").html(window.applicationCache.status);
+        // $("#cachestatus").html(window.applicationCache.status);
     }, false);
     
     window.applicationCache.update();
