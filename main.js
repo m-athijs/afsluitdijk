8e54266a1ebadda66e7b81b2fc2a6b43cb313f19
Mathijs van Bockhooven <mathijs@Mathijss-MacBook-Pro.local>
2017-10-28 20:10:03 +0200

    testing device motion detection

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
         // }
         // $("#cachestatus").html(window.applicationCache.status);
     }, false);
-    
+
     window.applicationCache.addEventListener('noupdate', function (e) {
         $("#status").html("<a href='audiotour1.html'>START AUDIO TOUR</a>");
         $("#language").hide();
@@ -28,7 +28,7 @@ $(document).ready(function () {
         // }
         // $("#cachestatus").html(window.applicationCache.status);
     }, false);
-    
+
     window.applicationCache.addEventListener('updateready', function (e) {
         $("#status").html("<a href='audiotour1.html'>START AUDIO TOUR</a>");
         $("#language").hide();
@@ -38,21 +38,21 @@ $(document).ready(function () {
         // }
         // $("#cachestatus").html(window.applicationCache.status);
     }, false);
-    
+
     window.applicationCache.addEventListener('checking', function (e) {
         // if (e.message != undefined) {
         //     $("#cacheevent").html("checking " + e.message);
         // }
         // $("#cachestatus").html(window.applicationCache.status);
     }, false);
-    
+
     window.applicationCache.addEventListener('downloading', function (e) {
         // if (e.message != undefined) {
         //     $("#cacheevent").html("downloading " + e.message);
         // }
         // $("#cachestatus").html(window.applicationCache.status);
     }, false);
-    
+
     window.applicationCache.addEventListener('error', function (e) {
         // $("#cacheevent").html("error " + e.message);
         // $("#cachestatus").html(window.applicationCache.status);
@@ -62,25 +62,28 @@ $(document).ready(function () {
             $("#status").show();
         }
     }, false);
-    
+
     window.applicationCache.addEventListener('obsolete', function (e) {
         // $("#cacheevent").html("obsolete " + e.message);
         // $("#cachestatus").html(window.applicationCache.status);
     }, false);
-    
+
     window.applicationCache.addEventListener('progress', function (e) {
         // $("#cacheevent").html("progress " + e.message);
         var percentageLoaded = Math.ceil(e.loaded / e.total * 100);
         $("#loaded").html(percentageLoaded);
         // $("#cachestatus").html(window.applicationCache.status);
     }, false);
-    
+
     window.applicationCache.update();
-    
+
     if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
         window.applicationCache.swapCache();
-    }    
+    }
+
+    if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion', function(e) {
+            alert('motion detected...');
+        });
+    }
 });
-
-
-
